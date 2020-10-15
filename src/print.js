221596b9c5725be609067a438a429f48da6fc775
zm9 <zm9@meitu.com>
2020-10-15 15:40:26 +0800

    commit1

--- src/print.js
+++ src/print.js
@@ -2,4 +2,5 @@ export default function printMe() {
     // console.log('I get called from print.js!');
     cosnole.error('I get called from print.js!');
     console.log('我是main分支的content');
+    console.log('commit1 ');
 }
