025e8dd9d511955cfa06000716e39ec81cb96c74
zm9 <zm9@meitu.com>
2020-10-15 19:27:30 +0800

    test

--- src/index.js
+++ src/index.js
@@ -2,23 +2,18 @@ import _ from 'lodash';
 // 需要配置loader才可以导入css后缀名的文件
 import './style.css';
 // import myPic from '../image/pig.jpg';
-import Data from './data.xml';
-import printMe from './print.js';
+// import Data from './data.xml';
+import  './another-module.js';
 
 
-console.log(Data);
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode');
+}
+
 let component = function () {
     let element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
-
-    let btn = document.createElement('button');
-    btn.innerHTML = 'btn';
-    btn.onclick = printMe;
-    element.appendChild(btn);
-    // let icon = new Image;
-    // icon.src= myPic;
-    // element.appendChild(icon);
     return element;
 }
 
