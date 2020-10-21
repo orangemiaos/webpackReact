94dce9756fca8e9aafd87f84d5757c748fdcc401
zm9 <zm9@meitu.com>
2020-10-21 13:57:39 +0800

    webpack

--- src/another.js
+++ src/another.js
@@ -1,3 +1,4 @@
-import _ from 'lodash';
 
-console.log(6666);
+// webpack自动设置环境变量，development，production
+console.log(6666, process.env.TEST);
+console.log(77, process.env.ASSET_PATH);  
--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
-// webpack-dev-server 实时更新修改的内容，不需要每次都重新build
+// webpack-dev-server 实时更新修改的内容，不需要每次都重新build。在内存中进行代码的编译和资源的提供，但并不写入磁盘来提高性能
 
 module.exports = {
     entry: {
@@ -14,7 +14,6 @@ module.exports = {
     output: {
         // name是文件名
         // chunk就模块，chunkhash也就是根据模块内容计算出的hash值
-        filename: '[name].[chunkhash].js',
         // hash是编译对象计算所得，而不是具体的项目文件计算所得。以上配置的编译输出文件，所有的文件名都会使用相同的hash指纹。
         // 如果有三个js文件，任何一个改动都会影响另外两个文件的最终文件名。上线后，另外两个文件的浏览器缓存也全部失效，所以尽量使用chunkhash
         path: path.resolve(__dirname, 'dist')
